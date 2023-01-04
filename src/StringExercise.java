3fef4265f7a35bb4cbece11dfab95f3efeec6c1e
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-01-04 16:28:43 -0600

    finished first question

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,12 @@
 public class StringExercise {
     public static void main(String[] args) {
-
+//        Todo 1 A
+        System.out.println("We don't need no education \n" + "We don't need no thought control");
+//        Todo 1 B
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+//        Todo 1 C
+        System.out.println("In windows, the main drive is usually C:\"");
+//        Todo 1 D
+        System.out.println("I can do backslashes \\, double backslashes \\\\,\n" + "and the amazing triple backslash \\\\\\!");
     }
 }
