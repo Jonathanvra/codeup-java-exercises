8ebe54170424666b3618b879b13b2addeb2039d9
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-01-10 22:02:47 -0600

    saving process on Rectangle

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,8 +1,27 @@
 package shapes;
 
-public class Rectangle {
-
+import util.Input;
+public class Rectangle{
+    double width;
+    double height;
+    Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+//  Todo getPerimeter
+    public double getPerimeter() {
+        return 2 * width + 2 * height;
+    }
+// Todo getArea
     public double getArea() {
-        return Math.PI * Math.pow(this.radius, 2);
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println("Let's get a radius for our circle:");
+        Rectangle rectangle = new Rectangle(input.getInt());
+        System.out.printf("The perimeter of the rectangle is %f; The area of a rectangle is %f.%n", Rectangle.getPerimeter(), Rectangle.getArea());
+//        System.out.printf("Circle's area: %f; Circle's circumference: %f.%n", Rectangle.getPerimeter(), Rectangle.getArea());
     }
 }
