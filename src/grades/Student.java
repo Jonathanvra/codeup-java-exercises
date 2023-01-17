9e830a1c72605b8eff57390185d53914c055b498
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-01-17 11:48:14 -0600

    saving progress for pair programming

---
+++ src/grades/Student.java
@@ -0,0 +1,27 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    // returns the student's name
+//    public String getName();
+//    // adds the given grade to the grades property
+//    public void addGrade(int grade);
+//    // returns the average of the students grades
+//    public double getGradeAverage();
+
+//    private String name;
+//    private String type;
+//    private String countryOfOrigin;
+//    private List<String> characteristics;
+
+//    public Cheese() {
+//    }
+
+//    public Cheese(String name, String type, String countryOfOrigin) {
+//        this.name = name;
+//        this.type = type;
+//        this.countryOfOrigin = countryOfOrigin;
+//        this.characteristics = new ArrayList<>();
+//    }
+}
--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -14,10 +14,17 @@ public class Circle {
     public double getCircumference() {
         return 2 * Math.PI * this.radius;
     }
-    public static void main(String[] args) {
-        Input input = new Input();
-        System.out.println("Enter the radius for our circle:");
-        Circle circle = new Circle(input.getDouble());
-        System.out.printf("The circle's area: %f; Circle's circumference: %f.%n", circle.getArea(), circle.getCircumference());
-    }
+//    public static void main(String[] args) {
+//        Input input = new Input();
+//        System.out.println("Enter the radius for our circle:");
+//        Circle circle = new Circle(input.getDouble());
+//        System.out.printf("The circle's area: %f; Circle's circumference: %f.%n", circle.getArea(), circle.getCircumference());
+//    }
+public static void main(String[] args) {
+    int[][] primes = new int[3][3];
+    primes[2][2] = 1;
+
+    System.out.println(primes[1][2]);
+}
+
 }
